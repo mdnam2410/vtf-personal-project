e70c7e3edeeef07b36a00197bfebe73ab1b146c9
Nam Mai <mdnam2410@gmail.com>
2022-07-05 15:31:16 +0700

    Add basic zombie AI

---
+++ Assets/MadProject/Scripts/ZombieAI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner;
+using BehaviorDesigner.Runtime;
+using UnityEngine;
+
+public class ZombieAI : MonoBehaviour
+{
+    [SerializeField]
+    private BehaviorTree _behaviorTree;
+
+    private SharedFloat _speed;
+    private SharedFloat _fieldOfView;
+    private GameObject _currentTarget;
+
+    private void OnValidate()
+    {
+        _behaviorTree = GetComponent<BehaviorTree>();
+    }
+
+    private void Start()
+    {
+        _speed = _behaviorTree.GetVariable("Speed") as SharedFloat;
+        _fieldOfView = _behaviorTree.GetVariable("FieldOfView") as SharedFloat;
+
+        _speed.Value = 1;
+        _fieldOfView.Value = 90;
+
+        _behaviorTree.EnableBehavior();
+    }
+
+    public void OnDeath()
+    {
+        _behaviorTree.SendEvent("IsDead");
+    }
+}
