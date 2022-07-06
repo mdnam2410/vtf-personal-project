da8b9043ad98c897351d4e26c0e1cee2ae9083ca
Nam Mai <mdnam2410@gmail.com>
2022-07-06 23:14:05 +0700

    Implement game logic

--- Assets/MadProject/Scripts/GameLogic.cs
+++ Assets/MadProject/Scripts/GameLogic.cs
@@ -11,25 +11,63 @@ public class GameLogic : MonoBehaviour
     [SerializeField]
     private List<GameObject> _victims;
 
+    [SerializeField]
+    private GameObject _victoryPanel;
+    [SerializeField]
+    private GameObject _defeatPanel;
+    [SerializeField]
+    private GameObject _backButton;
+
+    private bool _gameEnd;
+
     private void LateUpdate()
     {
-        //if (_zombieManager.DeadZombiesCount == _zombieManager.MaxZombies)
-        //{
-        //    Debug.Log("Player wins");
-        //}
-        //else if (_player.GetComponent<HP>().CurrentHP == 0)
-        //{
-        //    Debug.Log("Zombie wins");
-        //}
-        //else
-        //{
-        //    for (int i = 0; i < _victims.Count; ++i)
-        //    {
-        //        if (_victims[i].GetComponent<HP>().CurrentHP == 0)
-        //        {
-        //            Debug.Log($"Zombie wins because victim {i} dies");
-        //        }
-        //    }
-        //}
+        if (_gameEnd) return;
+
+        bool victory = false;
+
+        // Player is killed
+        if (_player.GetComponent<HP>().CurrentHP == 0)
+        {
+            _gameEnd = true;
+        }
+        // All zombies are killed
+        else if (_zombieManager.DeadZombiesCount == _zombieManager.MaxZombies)
+        {
+            _gameEnd = true;
+            victory = true;
+        }
+        else
+        {
+            // All victims are killed
+            for (int i = 0; i < _victims.Count; ++i)
+            {
+                if (_victims[i].GetComponent<HP>().CurrentHP == 0)
+                {
+                    victory = false;
+                    _gameEnd = true;
+                    break;
+                }
+            }
+        }
+
+        if (!_gameEnd) return;
+
+        Time.timeScale = 0;
+        _backButton.SetActive(true);
+        if (victory)
+            OnVictory();
+        else
+            OnDefeat();
+    }
+
+    private void OnVictory()
+    {
+        _victoryPanel.SetActive(true);
+    }
+
+    private void OnDefeat()
+    {
+        _defeatPanel.SetActive(true);
     }
 }
