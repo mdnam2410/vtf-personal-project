8e4b14ce2fbaa9eb23448216bb46abd814dd9d4a
Nam Mai <mdnam2410@gmail.com>
2022-07-06 19:34:44 +0700

    Link welcome scene and main scene

--- Assets/MadProject/Scripts/AdditiveSceneLoader.cs
+++ Assets/MadProject/Scripts/AdditiveSceneLoader.cs
@@ -9,25 +9,32 @@ public class AdditiveSceneLoader : MonoBehaviour
     [SerializeField]
     private GameObject _playButton;
     [SerializeField]
-    private Button _backButton;
+    private GameObject _backButton;
     [SerializeField]
     private GameObject _pausePanel;
+    [SerializeField]
+    private GameObject _pauseButton;
+    [SerializeField]
+    private GameObject _cancelButton;
 
     private void OnValidate()
     {
-        _backButton = _pausePanel.GetComponentInChildren<Button>();
+        //_backButton = _pausePanel.GetComponentInChildren<Button>();
     }
 
     private void Start()
     {
         _playButton.GetComponent<Button>().onClick.AddListener(LoadMainScene);
-        _backButton.onClick.AddListener(BackToMainMenu);
+        _backButton.GetComponent<Button>().onClick.AddListener(BackToMainMenu);
+        _pauseButton.GetComponent<Button>().onClick.AddListener(OnPauseButtonClick);
+        _cancelButton.GetComponent<Button>().onClick.AddListener(OnCancelButtonClick);
     }
 
     private void LoadMainScene()
     {
         SceneManager.LoadScene("Main", LoadSceneMode.Additive);
         _playButton.SetActive(false);
+        _pauseButton.SetActive(true);
         Time.timeScale = 1;
     }
 
@@ -37,17 +44,34 @@ public class AdditiveSceneLoader : MonoBehaviour
         //SceneManager.LoadScene("Welcome", LoadSceneMode.Additive);
         _playButton.SetActive(true);
         _pausePanel.SetActive(false);
+        _pauseButton.SetActive(false);
+    }
+
+    private void OnPauseButtonClick()
+    {
+        _pausePanel.SetActive(true);
+        Time.timeScale = 0;
+        FindObjectOfType<FPSMouseController>().enabled = false;
+        _pauseButton.SetActive(false);
+    }
+
+    private void OnCancelButtonClick()
+    {
+        _pausePanel.SetActive(false);
+        _pauseButton.SetActive(false);
+        Time.timeScale = 1;
+        FindObjectOfType<FPSMouseController>().enabled = true;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            _pausePanel.SetActive(!_pausePanel.activeInHierarchy);
-            Cursor.lockState = _pausePanel.activeInHierarchy ? CursorLockMode.None : CursorLockMode.Locked;
-            Time.timeScale = _pausePanel.activeInHierarchy ? 0 : 1;
-            FindObjectOfType<FPSMouseController>().enabled = !_pausePanel.activeInHierarchy;
-        }
+        //if (Input.GetKeyDown(KeyCode.P))
+        //{
+        //    _pausePanel.SetActive(!_pausePanel.activeInHierarchy);
+        //    Cursor.lockState = _pausePanel.activeInHierarchy ? CursorLockMode.None : CursorLockMode.Locked;
+        //    Time.timeScale = _pausePanel.activeInHierarchy ? 0 : 1;
+        //    FindObjectOfType<FPSMouseController>().enabled = !_pausePanel.activeInHierarchy;
+        //}
 
         //Input.GetA
     }
