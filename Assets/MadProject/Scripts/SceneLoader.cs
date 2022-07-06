d2000403f45835847088b06cdb324a85cf1b6e5c
Nam Mai <mdnam2410@gmail.com>
2022-07-06 20:03:23 +0700

    WIP

--- Assets/MadProject/Scripts/SceneLoader.cs
+++ Assets/MadProject/Scripts/SceneLoader.cs
@@ -17,21 +17,42 @@ public class SceneLoader : MonoSingleton<SceneLoader>
     [SerializeField]
     private GameObject _cancelButton;
 
+    private const int PausePanelUILayer = 10;
+
     private void Start()
     {
         _playButton.GetComponent<Button>().onClick.AddListener(LoadMainScene);
-        _backButton.GetComponent<Button>().onClick.AddListener(BackToMainMenu);
-        _pauseButton.GetComponent<Button>().onClick.AddListener(OnPauseButtonClick);
-        _cancelButton.GetComponent<Button>().onClick.AddListener(OnCancelButtonClick);
     }
 
     private void LoadMainScene()
     {
         SceneManager.LoadScene("Main");
+        FindPausePanel();
         _playButton.SetActive(false);
         _pauseButton.SetActive(true);
+        _pausePanel.SetActive(true);
         Time.timeScale = 1;
     }
+
+    private void FindPausePanel()
+    {
+        //GameObject[] objects = FindObjectsOfType<GameObject>();
+        //for (int i = 0; i < objects.Length; ++i)
+        //{
+        //    if (objects[i].layer == PausePanelUILayer)
+        //    {
+        //        GameObject.Find("")
+        //    }
+        //}
+        _pausePanel = GameObject.Find("PausePanel");
+        _pauseButton = GameObject.Find("PauseButton");
+        _backButton = GameObject.Find("BackToMainMenuButton");
+        _cancelButton = GameObject.Find("CancelButton");
+        _backButton.GetComponent<Button>().onClick.AddListener(BackToMainMenu);
+        _pauseButton.GetComponent<Button>().onClick.AddListener(OnPauseButtonClick);
+        _cancelButton.GetComponent<Button>().onClick.AddListener(OnCancelButtonClick);
+    }
+
 
     private void BackToMainMenu()
     {
